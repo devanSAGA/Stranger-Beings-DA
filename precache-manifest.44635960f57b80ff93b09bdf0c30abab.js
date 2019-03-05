0e87e2b26292294bd638ec7875e9e8ef59ceb38d
Devansh Purohit <officialdevansh@gmail.com>
2019-03-06 00:43:02 +0530

    Updates

---
+++ precache-manifest.44635960f57b80ff93b09bdf0c30abab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Stranger-Beings-DA/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "efdab85e99b92ac04177",
+    "url": "/Stranger-Beings-DA/static/js/runtime~main.efdab85e.js"
+  },
+  {
+    "revision": "39a51b49b50ac2b72284",
+    "url": "/Stranger-Beings-DA/static/js/main.39a51b49.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/Stranger-Beings-DA/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "39a51b49b50ac2b72284",
+    "url": "/Stranger-Beings-DA/static/css/main.021ca7e4.chunk.css"
+  },
+  {
+    "revision": "82a9ae9918b2412da4298a5ef8cd2e44",
+    "url": "/Stranger-Beings-DA/index.html"
+  }
+];
