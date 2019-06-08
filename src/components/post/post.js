3971f9ccab75407b456775dfbc1f71d529aa11a5
Devansh <officialdevansh@gmail.com>
2019-06-08 20:03:09 +0530

    implement common layout pattern

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -1,59 +1,61 @@
-import React from "react";
-import "./post.css";
+import React from 'react';
+import './post.css';
 
 class Post extends React.Component {
-
   getLayout = () => {
-    let width = window.innerWidth
-    || document.documentElement.clientWidth
-    || document.body.clientWidth;
+    let width =
+      window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.body.clientWidth;
     console.log(width);
-    if(this.props.alignment && width>=710){
-      // if(this.props.src){
-      //   return <img src={this.props.src} alt="funny_image" />
-      // }
-      // if(this.props.description){
-      //   return <div className={"description " + (this.props.src ? "" : "no-image")}>{this.props.description}</div>
-      // }
+    if (this.props.alignment && width >= 710) {
       return (
-      <div
-        className={"post-container " + this.getLayoutStyle(this.props.layout)}
-      >
-          {this.props.description && <div className={"description " + (this.props.src ? "" : "no-image")}>{this.props.description}</div>}
+        <div
+          className={'post-container ' + this.getLayoutStyle(this.props.layout)}
+        >
+          {this.props.description && (
+            <div
+              className={'description ' + (this.props.src ? '' : 'no-image')}
+            >
+              {this.props.description}
+            </div>
+          )}
           {this.props.src && <img src={this.props.src} alt="funny_image" />}
-      </div>
+        </div>
       );
     } else {
       return (
         <div
-          className={"post-container " + this.getLayoutStyle(this.props.layout)}
+          className={'post-container ' + this.getLayoutStyle(this.props.layout)}
         >
-            {this.props.src && <img src={this.props.src} alt="funny_image" />}
-            {this.props.description && <div className={"description " + (this.props.src ? "" : "no-image")}>{this.props.description}</div>}
+          {this.props.src && <img src={this.props.src} alt="funny_image" />}
+          {this.props.description && (
+            <div
+              className={'description ' + (this.props.src ? '' : 'no-image')}
+            >
+              {this.props.description}
+            </div>
+          )}
         </div>
-        );
-    } 
-  }
+      );
+    }
+  };
 
   getLayoutStyle = layout => {
     switch (layout) {
-      case "Horizontal":
-        return "horizontal-image";
+      case 'Horizontal':
+        return 'horizontal-image';
 
-      case "Vertical":
-        return "vertical-image";
+      case 'Vertical':
+        return 'vertical-image';
 
       default:
-        return "image-container";
+        return 'image-container';
     }
   };
 
   render() {
-    return (
-      <React.Fragment>
-        {this.getLayout()}
-      </React.Fragment>
-    );
+    return <React.Fragment>{this.getLayout()}</React.Fragment>;
   }
 }
 
