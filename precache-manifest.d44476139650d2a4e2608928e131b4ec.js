4fa70c17f2089299954b3631311a01b3d5d768da
Devansh <officialdevansh@gmail.com>
2019-06-10 23:50:30 +0530

    Updates

---
+++ precache-manifest.d44476139650d2a4e2608928e131b4ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "efdab85e99b92ac04177",
+    "url": "/Stranger-Beings-DA/static/js/runtime~main.efdab85e.js"
+  },
+  {
+    "revision": "54277d6c2b75938912a1",
+    "url": "/Stranger-Beings-DA/static/js/main.54277d6c.chunk.js"
+  },
+  {
+    "revision": "62c5804ff224d8643ade",
+    "url": "/Stranger-Beings-DA/static/js/2.62c5804f.chunk.js"
+  },
+  {
+    "revision": "54277d6c2b75938912a1",
+    "url": "/Stranger-Beings-DA/static/css/main.3ee30495.chunk.css"
+  },
+  {
+    "revision": "62c5804ff224d8643ade",
+    "url": "/Stranger-Beings-DA/static/css/2.6a60b82c.chunk.css"
+  },
+  {
+    "revision": "33df490964d615ec67050bef045adb3c",
+    "url": "/Stranger-Beings-DA/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Stranger-Beings-DA/precache-manifest.086a7228844f2e311269c650a3903a43.js"
+  "/Stranger-Beings-DA/precache-manifest.d44476139650d2a4e2608928e131b4ec.js"
 );
 
 workbox.clientsClaim();
