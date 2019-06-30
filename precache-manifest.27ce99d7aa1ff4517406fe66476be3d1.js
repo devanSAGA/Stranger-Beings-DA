3cd331933095146ee8f26f2f1171167b45419a90
Rutul Patel <rutup@amazon.com>
2019-06-30 15:37:34 +0530

    Updates

---
+++ precache-manifest.27ce99d7aa1ff4517406fe66476be3d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "efdab85e99b92ac04177",
+    "url": "/Stranger-Beings-DA/static/js/runtime~main.efdab85e.js"
+  },
+  {
+    "revision": "b531c7c0226c40769e05",
+    "url": "/Stranger-Beings-DA/static/js/main.b531c7c0.chunk.js"
+  },
+  {
+    "revision": "bf8fb23c45b1bc8d066b",
+    "url": "/Stranger-Beings-DA/static/js/2.bf8fb23c.chunk.js"
+  },
+  {
+    "revision": "b531c7c0226c40769e05",
+    "url": "/Stranger-Beings-DA/static/css/main.f34d25a6.chunk.css"
+  },
+  {
+    "revision": "bf8fb23c45b1bc8d066b",
+    "url": "/Stranger-Beings-DA/static/css/2.7ac7d421.chunk.css"
+  },
+  {
+    "revision": "4cf5fce9becdf4f37656d8a72c30eec2",
+    "url": "/Stranger-Beings-DA/index.html"
+  }
+];
